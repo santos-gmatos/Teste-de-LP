edc0cf5c10f839bc20fc2e45dcba7faf52b23001
GABRIEL MATOS DOS SANTOS <B51109@fgv.edu.br>
2023-09-20 07:47:34 -0300

    final update

--- Teste-de-LP/SourceDate.py
+++ Teste-de-LP/SourceDate.py
@@ -1,8 +1,9 @@
+import doctest
 from datetime import datetime 
 
 def get_date(date_input):
     """
-    Calcula a diferença entre duas datas
+    Calcula a diferença entre duas datas.
 
     Parameters
     ----------
@@ -12,7 +13,14 @@ def get_date(date_input):
     Retorna a diferença entre as datas!
     -------
     None.
-
+    
+    -------
+    >>> get_date("28 de Agosto de 2023 - 29 de Agosto de 2023")
+    1
+    
+    >>> get_date("20 de Agosto de 2022 - 29 de Agosto de 2023")
+    374
+    
     """
     nlist_1 = [int(i) for i in date_input.split() if i.isdigit()]
     if len(nlist_1) != 4:
@@ -26,3 +34,7 @@ def get_date(date_input):
     data_1 = datetime(nlist_1[1], months[nlist_2[0]], nlist_1[0])
     data_2 = datetime(nlist_1[3], months[nlist_2[1]], nlist_1[2])
     return(abs((data_1 - data_2).days))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
